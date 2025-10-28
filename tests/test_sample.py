521997c819dcc57ca7fad265d9785a335f7689eb
“myfirstgitravindra” <mynaukari1010@gmail.com>
2025-10-28 13:40:44 +0530

    Add sample test files

---
+++ tests/test_sample.py
@@ -0,0 +1,7 @@
+def test_example():
+    """Sample test that always passes"""
+    assert 1 + 1 == 2
+
+def test_another_example():
+    """Another sample test"""
+    assert "hello".upper() == "HELLO"
